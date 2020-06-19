7978dc4db92bd618588bdc1d1aa04474a2bbf1d2
DattaDhebe <dattadhebe75@gmail.com>
2020-06-19 18:57:19 +0530

    Added : 'TC-1.3' To Check Given two Diffrent Values should return Not Eqaul.

--- Quantity Measurements Testing/QuantityMeasurementsTesting.cs
+++ Quantity Measurements Testing/QuantityMeasurementsTesting.cs
@@ -25,7 +25,7 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// Given first Feet 0 and second Feet 0 when should return Equal
+        /// TC-1.1 : Given first Feet 0 and second Feet 0 when should return Equal
         /// </summary>
         [Test]
         public void Given0FeetAnd0Feet_ShouldReturnEqual()
@@ -36,14 +36,25 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// Given first Feet Value and second Feel Null should return Not Equal
+        /// TC-1.2 : Given first Feet Value and second Feet Null should return Not Equal
         /// </summary>
         [Test]
-        public void Given0FeetAndNull_ShouldReturnEqual()
+        public void Given0FeetAndNull_ShouldReturnNotEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = null;
             Assert.AreNotEqual(feet1, feet2);
-        }       
+        }
+
+        /// <summary>
+        /// TC-1.3 : Given first Feet 0 and second Feet 2 when should return Equal
+        /// </summary>
+        [Test]
+        public void Given0FeetAnd2Feet_ShouldReturnNotEqual()
+        {
+            Feet feet1 = new Feet(0.0);
+            Feet feet2 = new Feet(2.0);
+            Assert.AreNotEqual(feet1, feet2);
+        }
     }
 }
