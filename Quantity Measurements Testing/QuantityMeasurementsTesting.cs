b813644f34e4f468e9ebb1159b8a2a220eb15ef9
DattaDhebe <dattadhebe75@gmail.com>
2020-06-20 00:29:44 +0530

    Added : 'TC-1.5' Given One Object to Another And Check Reference.

--- Quantity Measurements Testing/QuantityMeasurementsTesting.cs
+++ Quantity Measurements Testing/QuantityMeasurementsTesting.cs
@@ -25,10 +25,10 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// TC-1.1 : Given first Feet 0 and second Feet 0 when should return Equal
+        /// TC-1.1 : Given first Feet 0 and second Feet 0 when checked should return Equal
         /// </summary>
         [Test]
-        public void Given0FeetAnd0Feet_ShouldReturnEqual()
+        public void Given0FeetAnd0Feet_WhenChecked_ShouldReturnEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = new Feet(0.0);
@@ -36,10 +36,10 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// TC-1.2 : Given first Feet Value and second Feet Null should return Not Equal
+        /// TC-1.2 : Given first Feet Value and second Feet Null when checked should return Not Equal
         /// </summary>
         [Test]
-        public void Given0FeetAndNull_ShouldReturnNotEqual()
+        public void Given0FeetAndNull_WhenChecked_ShouldReturnNotEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = null;
@@ -47,10 +47,10 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// TC-1.3 : Given first Feet 0 and second Feet 2 should return Not Equal
+        /// TC-1.3 : Given first Feet 0 and second Feet 2 when checked should return Not Equal
         /// </summary>
         [Test]
-        public void Given0FeetAnd2Feet_ShouldReturnNotEqual()
+        public void Given0FeetAnd2Feet_WhenChecked_ShouldReturnNotEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = new Feet(2.0);
@@ -58,14 +58,26 @@ namespace Quantity_Measurements_Testing
         }
 
         /// <summary>
-        /// TC-1.4 : Given first Feet Object and second Feet Object should return Equal
+        /// TC-1.4 : Given first Feet Object and second Feet Object when checked should return Equal
         /// </summary>
         [Test]
-        public void GivenFeetObjectAndFeetObject_ShouldReturnNotEqual()
+        public void GivenFeetObjectAndFeetObject_WhenChecked_ShouldReturnNotEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = new Feet(2.0);
             Assert.AreEqual(feet1.GetType(), feet2.GetType());
+        }
+
+        /// <summary>
+        /// TC-1.5 : Given first Feet Object and second Feet Object when checked should return Equal
+        /// </summary>
+        [Test]
+        public void GivenTwoObjectHaveSameReference_WhenCheckedReference_ShouldReturnNotEqual()
+        {
+            Feet feet1 = new Feet(0.0);
+            Feet feet2 = feet1;
+            bool check = System.Object.ReferenceEquals(feet1, feet2);
+            Assert.True(check);
         }
     }
 }
